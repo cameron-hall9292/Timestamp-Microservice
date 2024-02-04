a89cf576103747240acd418ff4776ce22fed7107
cameron-hall9292 <cameron.hall9292@gmail.com>
2024-02-04 13:59:16 -0600

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 
+//add a port
+
+const PORT = process.env.PORT || 5500;
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -20,13 +24,57 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+
+  let userInput = req.params.date;
+  
+   // variable for when regular date type is input
+  let date = new Date(userInput);
+
+  let dateToUTC = date.toUTCString()
+
+  let unixFromRegDate = parseInt(date.getTime() / 1000);
+
+  unixFromRegDate *= 1000;
+
+
+
+// variable for when unix date type is input
+  let unixInput = parseInt(userInput);
+  let dateFromUnix = new Date(unixInput).toUTCString();
+
+if (dateToUTC !== "Invalid Date"){
+ 
+    res.json({unix: unixFromRegDate ,utc: dateToUTC});
+}
+
+else if (dateFromUnix !== "Invalid Date"){
+
+
+    res.json({unix: unixInput ,utc: dateFromUnix});
+}
+
+else {
+    res.json({error : "Invalid Date"});
+}
+
+
+}); 
+
+
+
+
+
+app.get("/api",function (req,res) {
+
+  res.json({unix: Date.now(),utc: new Date().toUTCString()});
+  
 });
 
 
 
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
